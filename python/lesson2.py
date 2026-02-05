da65850c3775b23b8ec4e89b773f0b4749b271bb
Amanda Bialer <amandabialer@gmail.com>
2026-02-05 19:30:55 -0300

    start L2

---
+++ python/lesson2.py
@@ -0,0 +1,21 @@
+name = input("Name: ")
+# print("Hello, " + name + "!")
+print(f"Hello, {name}!")
+
+
+
+n = int(input("Number: "))
+if n > 0:
+    print("Number is Positive")
+elif n < 0:
+    print("Number is Negative")
+else:
+    print("Number is equals to zero")
+    
+    
+
+name = "John"
+print(name[0])  # J
+
+list = ["apple", "banana", "cherry"]]
+print(list[0])  # apple
